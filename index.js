2bac2cdcd5d4948e08fc197c0fd28996290fecf7
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-20 18:42:44 -0600

    functional syntax

--- index.js
+++ index.js
@@ -14,7 +14,33 @@ function shouter(someString){
     return loudString + "!!!";
 }
 
-let goToBedLittleBaby = "9684961596159641";
+const goToBedLittleBaby = "go to sleep";
 console.log(shouter(goToBedLittleBaby));
 
+function add(x, y){
+    return x + y;
+}
 
+console.log("Add Function", add(1, 2));
+
+let subtract = function(x, y){
+    return x - y;
+}
+
+console.log("Subtract Function", subtract(2, 1));
+
+let multiply = function(x, y){
+    return x * y;
+}
+
+console.log("Multiply Function", multiply(2, 3));
+
+let divide = (theCarIsSoSlow, theMotorcycleIsVeryFast) => theCarIsSoSlow / theMotorcycleIsVeryFast;
+
+const x = 10;
+const y = 2;
+
+console.log("Divide Function", divide(x, y));
+
+console.log(x)
+console.log(y)
