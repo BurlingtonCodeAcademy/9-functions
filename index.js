3a8ea2bcfcb1cba7f2afbe059720506cdc2295a8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-20 18:52:51 -0600

    fizzBuzz example

--- index.js
+++ index.js
@@ -44,3 +44,39 @@ console.log("Divide Function", divide(x, y));
 
 console.log(x)
 console.log(y)
+
+function fizzBuzz(number){
+    // some code
+    for(let i = 0; i <= number; i++){
+        if(i % 15 === 0){
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0){
+            console.log("Fizz");
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz(15);
+
+function fizzBuzzRecursive(num, maxNumber){
+    if(num > maxNumber){
+        return;
+    }
+
+    if(num % 15 === 0){
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0){
+        console.log("Fizz");
+    } else if (num % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+    fizzBuzzRecursive(num + 1, maxNumber);
+}
+
+fizzBuzzRecursive(1, 15);
