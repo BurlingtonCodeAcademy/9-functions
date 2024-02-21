38557273e84484daf0197acfb194202a6788a079
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-20 18:22:08 -0600

    sayHello and shouter examples

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello team! happy presidents day!")
+function sayHello(name){
+    let greeting = "Hello " + name;
+
+    return greeting;
+}
+
+let newGreeting = sayHello("John");
+
+console.log(newGreeting);
+
+function shouter(someString){
+    let theString = someString.toString();
+    let loudString = theString.toUpperCase();
+    return loudString + "!!!";
+}
+
+let goToBedLittleBaby = "9684961596159641";
+console.log(shouter(goToBedLittleBaby));
+
+
